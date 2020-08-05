1f4b1f7a5769b4b5a13f92dac2135c444385b551
Do Binh Huy <33474207+dbhuy2268@users.noreply.github.com>
2020-08-05 10:59:21 +0700

    add DTO

---
+++ WindowsFormsApp2/DTO/NhaCungCapDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class NhaCungCapDTO
+    {
+        public int maNhaCungCap { get; set; }
+        public string tenNhaCungCap { get; set; }
+
+        public NhaCungCapDTO(int maNhaCungCap, string tenNhaCungCap)
+        {
+            this.maNhaCungCap = maNhaCungCap;
+            this.tenNhaCungCap = tenNhaCungCap;
+        }
+    }
+}
